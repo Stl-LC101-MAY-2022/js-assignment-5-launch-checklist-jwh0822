fcb77e4396492423010a1f7d778cbf45545983a1
Joe Henke <jwhenke08@gmail.com>
2022-08-02 21:54:11 -0500

    Assignment 5 First Commit

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-// Write your JavaScript code here!
+// Write your JavaScript code 
 
 window.addEventListener("load", function() {
-
+    let form = document.querySelector('form');
+    form.addEventListener("submit", function(event){
+        let pilotName = document.querySelector("input[name=pilotName]");
+        pilotName = pilotName.value;
+        let copilotName = document.querySelector("input[name=copilotName]");
+        copilotName = copilotName.value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        fuelLevel = fuelLevel.value;
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        cargoMass = cargoMass.value;
+        let doc1 = document.documentElement;
+        let formList = document.getElementById('faultyItems');
+        let formSubResponse = formSubmission(doc1,formList,pilotName,copilotName,fuelLevel,cargoMass);
+        if(formSubResponse === 'Abort'){
+            event.preventDefault();
+            alert("Form Submission Aborted!");
+        }//end if
+        
+        if(formStatus !=="Abort"){
+            let div1 = document.getElementById('faultyItems');
+            
+            let list1 = document.getElementById('pilotStatus');
+            list1.style.visibility = 'visible';
+            list1.innerHTML += `Pilot Name:${pilotName.value}`;
+            let list2 = document.getElementById('copilotStatus');
+            list2.style.visibility = 'visible';
+            list2.innerHTML += `Pilot Name:${copilotName.value}`;
+        }
+    });    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -12,5 +40,4 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,69 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
-}
+   if(testInput ===''){
+        let inputResponse = "Empty";
+        return inputResponse;
+   }else if(isNaN(testInput)){
+        let inputResponse = 'Not a Number';
+        return inputResponse;
+    }else {
+        let inputResponse = 'Is a Number';
+        return inputResponse;
+     }//endifstatement
+}//eof
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let userInput = [pilot,copilot,fuelLevel,cargoLevel];
+    let validInputResponse = [];
+    let formStatus ='Yes';
+
+    for(let i = 0; i<=3; i++){
+        validInputResponse.push(validateInput(userInput[i]));
+    }//endforloop
+
+    if(validInputResponse.includes("Empty")){
+        alert("All fields must be filled!")
+        list.style.visibility = 'hidden';
+        formStatus = "Abort";
+        return formStatus;
+    }//endif
+
+    if(validInputResponse[0] === 'Is a Number' || validInputResponse[1] === 'Is a Number'){
+        alert("Pilot & Copilot Fields Must Be A Name!");
+        list.style.visibility = 'hidden';
+        formStatus = "Abort";
+        return formStatus;
+    }//endif
+
+    if(validInputResponse[2] === 'Not a Number' || validInputResponse[3] === 'Not a Number'){
+        alert("Fuel Level & Cargo Mass fields Must be a Number!");
+        list.style.visibility = 'hidden';
+        formStatus = 'Abort';
+        return formStatus;
+    }//endif
+
+    if(fuelLevel < 10000){
+        list.style.visibility = 'visible';
+        alert("Fuel to Low");
+        formStatus = 'Abort';
+        return formStatus;
+    }//endif
+
+    if(cargoLevel > 10000){
+        list.style.visibility = 'visible';
+        alert("Cargo Mass to High");
+        formStatus = 'Abort';
+        return formStatus;
+    }//endif
+
+    if(formStatus === 'Yes'){
+        list.style.visibility = 'visible';
+        alert("Form Good!");
+        formStatus = 'Yes';
+        return formStatus;
+    }//endif
+    
 }
 
 async function myFetch() {
